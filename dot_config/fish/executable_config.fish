2e451423d9b819414dc7c073f279d8f337e633b2
sodiel <detopuse@gmail.com>
2026-02-22 14:52:47 +0300

    Initial commit

---
+++ dot_config/fish/executable_config.fish
@@ -0,0 +1,43 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+
+    # Aliases
+    alias pamcan pacman
+    alias ls 'eza --icons'
+    alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias q 'qs -c ii'
+    
+end
+
+# pnpm
+set -gx PNPM_HOME "/home/user/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+set -gx ANDROID_HOME "$HOME/Android/Sdk"
+set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/platform-tools $PATH
+# zoxide 
+zoxide init fish --cmd cd | source
+# EDITOR = nvim
+set -gx EDITOR nvim
+# Переопределяем системный SDK на локальный
+# set -gx ANDROID_HOME "/opt/android-sdk"
+# set -gx PATH $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/platform-tools (string match -v "/opt/android-sdk*" $PATH)
+
